03bbee67c5229b16e1a334b9b8c79a4f3023918b
Mircea Nistor <mirceanis@gmail.com>
2019-12-03 15:00:33 +0100

    feat: only revoke once

--- contracts/RevocationRegistry.sol
+++ contracts/RevocationRegistry.sol
@@ -5,9 +5,13 @@ contract RevocationRegistry {
     mapping(bytes32 => mapping(address => uint)) private revocations;
 
     function revoke(bytes32 digest) public returns (bool) {
-        revocations[digest][msg.sender] = block.number;
-        emit Revoked(msg.sender, digest);
-        return true;
+        if (revocations[digest][msg.sender] == 0) {
+            revocations[digest][msg.sender] = block.number;
+            emit Revoked(msg.sender, digest);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     function revoked(address party, bytes32 digest) public view returns (uint) {
--- test/basic.js
+++ test/basic.js
@@ -10,11 +10,9 @@ contract('RevocationRegistry', function(accounts) {
 
   describe("revoked", () => {
     it("Should throw an invalid value when digest input longer than 32 bytes", async () => {
+      const digest = "0xaabbccddeeff00112233445566778899aabbccddeeff0011223344556677889900"
       try {
-        await registryInstance.revoked.call(
-                        accounts[0],
-                        "0xaabbccddeeff00112233445566778899aabbccddeeff0011223344556677889900"
-                      )
+        await registryInstance.revoked.call( accounts[0], digest )
       } catch (e) {
         assert.equal( e.message,
                       'invalid bytes32 value (arg="digest", coderType="bytes32", value="0xaabbccddeeff00112233445566778899aabbccddeeff0011223344556677889900")'
@@ -24,10 +22,7 @@ contract('RevocationRegistry', function(accounts) {
 
     it("Should throw an invalid value when digest is not a byte array", async () => {
       try {
-        await registryInstance.revoked.call(
-                        accounts[0],
-                        "some non hex string"
-                      )
+        await registryInstance.revoked.call( accounts[0], "some non hex string" )
       } catch (e) {
         assert.equal( e.message,
                       'invalid bytes32 value (arg="digest", coderType="bytes32", value="some non hex string")'
@@ -36,30 +31,43 @@ contract('RevocationRegistry', function(accounts) {
     });
 
     it("Should return 0 for non revoked digest", async () => {
-      let result = await registryInstance.revoked.call( accounts[0], "0xaabbccddeeff00112233445566778899aabbccddeeff00112233445566778899" )
+      const digest = "0xaabbccddeeff00112233445566778899aabbccddeeff00112233445566778899"
+      let result = await registryInstance.revoked.call( accounts[0], digest )
       assert.equal(result.toNumber(), 0)
     });
 
     it("Should return block number for revoked digest", async () => {
-      let tx = await registryInstance.revoke("0xabcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789",
+      const digest = "0xabcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789"
+      let tx = await registryInstance.revoke(digest,
         {
           from: accounts[0]
         })
 
-      let result = await registryInstance.revoked.call( accounts[0], "0xabcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789" )
+      let result = await registryInstance.revoked.call( accounts[0], digest )
       assert.isAbove(result.toNumber(), 0)
     });
 
     it("Should create an event during revocation", async () => {
-      let tx = await registryInstance.revoke("0xabcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789",
-        {
-          from: accounts[0]
-        })
+      const digest = "0xabcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789"
+      let tx = await registryInstance.revoke(digest, { from: accounts[0] })
       let event = tx.logs[0]
 
       assert.equal(event.event, "Revoked");
       assert.equal(event.args.revoker, accounts[0]);
-      assert.equal(event.args.digest, "0xabcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789");
+      assert.equal(event.args.digest, digest);
+    });
+
+    it("Should not revoke same digest twice", async () => {
+      const digest = "0xabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"
+      let tx1 = await registryInstance.revoke( digest, { from: accounts[0] })
+      let result1 = await registryInstance.revoked.call( accounts[0], digest )
+
+      let tx2 = await registryInstance.revoke( digest, { from: accounts[0] })
+      let result2 = await registryInstance.revoked.call( accounts[0], digest )
+
+      assert.isEmpty(tx2.logs)
+
+      assert.equal(result1.toNumber(), result2.toNumber())
     });
   })
 
