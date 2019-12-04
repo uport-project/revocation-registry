bd063f40d501bed4906d55909cc2ded1f789b3e4
Mircea Nistor <mirceanis@gmail.com>
2019-12-04 15:02:33 +0100

    feat: simplify revoke method signature
    `onlyOnce` modifier is not needed until more methods can benefit from it

--- contracts/RevocationRegistry.sol
+++ contracts/RevocationRegistry.sol
@@ -4,12 +4,8 @@ contract RevocationRegistry {
 
     mapping(bytes32 => mapping(address => uint)) private revocations;
 
-    modifier onlyOnce(bytes32 digest) {
+    function revoke(bytes32 digest) public {
         require (revocations[digest][msg.sender] == 0);
-        _;
-    }
-
-    function revoke(bytes32 digest) public onlyOnce(digest) {
         revocations[digest][msg.sender] = block.number;
         emit Revoked(msg.sender, digest);
     }
